296bfac96f0658eedf1550e8f4eec05e9ebab1be
Marvin Martinez <marvinmartinez579@gmail.com>
2017-04-02 16:07:24 -0500

    testing

--- js/main.js
+++ js/main.js
@@ -2,6 +2,6 @@
 var header = '<div class="ui menu"><a href="index.html" class="item">Home</a><!--    <a href="#" class="item" display="none">Profile</a>--><a href="resources.html" class="item">Resources</a><a href="faq.html" class="item">FAQ</a><div class="right menu"><!--    <a href="#" class="item">Login</a><a href="#" class="item">Sign Up</a>--></div></div>';
 
 $('document').ready(function() {
-  console.log("Success");
+  console.log("hello");
   $('.navBar').html(header);
 });
